182d2c3847aad5b4e03225cea04faa3b6334055a
John Flynn <40461634+Fullsailer@users.noreply.github.com>
2023-12-06 22:59:48 -0500

    adding portfolio porject folders and files to repo

---
+++ JS/site.js
@@ -0,0 +1,6 @@
+// Function to redirect button click.
+function redirectToNewPage() {
+    window.location.href= "/PortfolioProject/About.html";
+}
+//Event listener for click of button.
+document.getElementById('redirectButton').addEventListener('click', redirectToNewPage);
